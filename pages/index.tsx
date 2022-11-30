00d06fae312cbfff656d9b39b56f3f0654fd7c89
thanarack <thanarackc@gmail.com>
2022-11-30 16:47:58 +0700

    Added layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
-import styles from '../styles/Home.module.css'
+import { Container, Grid, GridItem } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <div>
-      Hi, Next is here.
+      <nav>Navbar</nav>
+      <Container as="main" maxWidth="1280">
+        <Grid templateColumns="repeat(12, 1fr)" gap={4}>
+          <GridItem colSpan={3}  h="10" bg="blue.500" />
+          <GridItem colSpan={6} h="10" bg="blue.500" />
+          <GridItem colSpan={3} h="10" bg="blue.500" />
+        </Grid>
+      </Container>
     </div>
-  )
+  );
 }
